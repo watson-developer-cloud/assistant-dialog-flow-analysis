c5f18d3d312da29d61e84c6d1179c396cbfbbb40
Avi <aviy@il.ibm.com>
2020-11-24 14:56:50 +0200

    feat: removed support for Python 3.5 end of service which doesn't support Pandas 1.1.x

--- setup.py
+++ setup.py
@@ -55,7 +55,6 @@ setuptools.setup(
         'License :: OSI Approved :: Apache Software License',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Operating System :: OS Independent',
